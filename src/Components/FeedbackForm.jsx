8dbcceb20960c11edb89e574dfaf977de9c58770
exyzte <davidecicerys@gmail.com>
2025-09-05 01:31:11 -0400

    fixed bugs and typos

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -12,54 +12,57 @@ const handleChange = (event) => {
             setFormData({
                 ...formData,
                 [name]: value
-            })
-}
+            });
+        };
 
-const handleSubmit = (event) {
-    event.preventDefault();
-    const confirmationMessage = `
-    Name: ${formData.name}
-    Email: ${formData.email}
-    Feedback: ${formData.feedback}
-    `;
-    const isConfirmed = window.confirm(`Please confirm yout details \n\n${confirmationMessage}`);
-    if(isConfirmed){
-        console.log('Submitting feedback: ', formData);
-        setFormData({
-            name: '',
-            email: '',
-            feedback: ''
-        });
-        alert('Thank you for your valuable feedback!');
-    }
-};
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const confirmationMessage = `
+        Name: ${formData.name}
+        Email: ${formData.email}
+        Feedback: ${formData.feedback}
+        `;
+        const isConfirmed = window.confirm(`Please confirm your details \n\n${confirmationMessage}`);
+        if(isConfirmed){
+            console.log('Submitting feedback: ', formData);
+            setFormData({
+                name: '',
+                email: '',
+                feedback: ''
+            });
+            alert('Thank you for your valuable feedback!');
+        }
+    };
 
   return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+      <form className="feedback-form" onSubmit={handleSubmit}>
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         <input 
             type="text"
             name="name"
             value={formData.name}
+            onChange={handleChange}
             placeholder="Your Name"
         />
         <input 
             type="email"
             name="email"
             value={formData.email}
+            onChange={handleChange}
             placeholder="Your Email"
         />
         <textarea
             name="feedback"
             value={formData.feedback}
+            onChange={handleChange}
             placeholder="Your Feedback"
         ></textarea>
-        <button type="submit" onClick={handleChange}>Submit Feedback</button>
+        <button className="feedback-form" type="submit">Submit Feedback</button>
       </form>
     </>
   );
